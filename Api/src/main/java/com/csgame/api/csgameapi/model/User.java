58c49ace341a36f4580f2e14a8548788d2593f7a
Sol Kumar <sohail.kumar03@gmail.com>
2022-11-19 14:17:40 -0500

    set up user.java, companyuser.java, npouser.java

--- Api/src/main/java/com/csgame/api/csgameapi/model/User.java
+++ Api/src/main/java/com/csgame/api/csgameapi/model/User.java
@@ -11,8 +11,33 @@ public class User {
     @JsonProperty("password")
     private String password;
 
-    public User(@JsonProperty("uid") int uid, @JsonProperty("username") String username) {
-        this.uid = uid;
+    public User(@JsonProperty("uid") int UID, @JsonProperty("username") String username, @JsonProperty("password") String password) {
+        this.UID = UID;
         this.username = username;
+        this.password = password;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String uID) {
+        UID = uID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
