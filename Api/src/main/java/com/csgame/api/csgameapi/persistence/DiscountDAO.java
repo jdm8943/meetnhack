5b90d02646888c94b3cb44e003b426a0ede9084e
Zak Kaufman <Zak3148@rit.edu>
2022-11-19 14:40:04 -0500

    added interface for discoundDAO

--- Api/src/main/java/com/csgame/api/csgameapi/persistence/DiscountDAO.java
+++ Api/src/main/java/com/csgame/api/csgameapi/persistence/DiscountDAO.java
@@ -1,3 +1,16 @@
-public class DiscountDAO {
+package com.csgame.api.csgameapi.persistance.DiscountDAO;
+
+import java.io.IOException;
+
+import com.csgame.api.csgameapi.model.Discount;
+
+public interface DiscountDAO {
     
+    Boolean getDiscount(int id) throws IOException;
+
+    Discount createDiscount(Discount discount) throws IOException;
+
+    Discount updateDiscount(Discount discount) throws IOException;
+
+    boolean deleteDiscount(Discount discount) throws IOException;
 }
