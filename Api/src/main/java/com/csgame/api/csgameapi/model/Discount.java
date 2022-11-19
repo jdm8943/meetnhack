73dd0a45bc65fef4e24f679db16a38e51524d1b0
Zak Kaufman <Zak3148@rit.edu>
2022-11-19 14:13:11 -0500

    Finished constructor for model of Discount

--- Api/src/main/java/com/csgame/api/csgameapi/model/Discount.java
+++ Api/src/main/java/com/csgame/api/csgameapi/model/Discount.java
@@ -1,3 +1,35 @@
+package com.csgame.api.csgameapi.model.Discount;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Discount {
+
+    public static final String STRING_FORMAT = "Discount [id=%d, name=%s, levelRequired=%d, pointsRequired=%d, companyId]";
+
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("levelRequired")
+    private int levelRequired;
+    @JsonProperty("pointsRequired")
+    private int pointsRequired;
+    @JsonProperty("companyId")
+    private int companyId;
+
+
+    @JsonCreator
+    public Discount(@JsonProperty("id") int id,
+    @JsonProperty("name") String name,
+    @JsonProperty("levelRequired") int levelRequired,
+    @JsonProperty("pointsRequired") int pointsRequired,
+    @JsonProperty("companyId") int companyId){
+        this.id = id;
+        this.name = name;
+        this.levelRequired = levelRequired;
+        this.pointsRequired = pointsRequired;
+        this.companyId = companyId;
+    }
     
 }
