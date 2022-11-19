2928de40db659a1427a3292372537322f7ddd1d1
Zak Kaufman <Zak3148@rit.edu>
2022-11-19 14:24:26 -0500

    Finished constructor for Discount Controller

--- Api/src/main/java/com/csgame/api/csgameapi/controller/DiscountController.java
+++ Api/src/main/java/com/csgame/api/csgameapi/controller/DiscountController.java
@@ -1,3 +1,20 @@
+package com.csgame.api.csgameapi.controller.DiscountController;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.csgame.api.csgameapi.persistance.DiscountDAO;
+
+@RestController
+@RequestMapping("discounts")
 public class DiscountController {
+    private static final Logger LOG = Logger.getLogger(UserController.class.getName());
+    private DiscountDAO discountDAO;
+
+    public DiscountController(DiscountDAO discountDAO) {
+        this.discountDAO = discountDAO;
+    }
+
     
 }
