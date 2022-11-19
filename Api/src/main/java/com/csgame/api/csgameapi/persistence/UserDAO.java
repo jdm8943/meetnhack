6da25bcae707d3c36e9f09920d9c588646b33b05
Spencer <sjk7876@rit.edu>
2022-11-19 14:18:32 -0500

    setup userDAO interface

--- Api/src/main/java/com/csgame/api/csgameapi/persistence/UserDAO.java
+++ Api/src/main/java/com/csgame/api/csgameapi/persistence/UserDAO.java
@@ -1,3 +1,11 @@
-public class UserDAO {
-    
+import java.io.IOException;
+
+public interface UserDAO {
+    User[] getUsers() throws IOException;
+
+    User getUser(String UID) throws IOException;
+
+    User updateUser(User updatedUser) throws IOException;
+
+    void login(User user) throws IOException;
 }
