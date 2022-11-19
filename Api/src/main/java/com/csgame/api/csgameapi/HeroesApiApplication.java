87268a6f575bc27f58fd4f43f645247ab218cf98
Sol Kumar <sohail.kumar03@gmail.com>
2022-11-19 14:21:56 -0500

    fix apiapp

--- Api/src/main/java/com/csgame/api/csgameapi/HeroesApiApplication.java
+++ Api/src/main/java/com/csgame/api/csgameapi/HeroesApiApplication.java
@@ -1,13 +1,13 @@
-package com.heroes.api.heroesapi;
+package com.csgame.api.csgameapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HeroesApiApplication {
+public class CSGameApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HeroesApiApplication.class, args);
+		SpringApplication.run(CSGameApiApplication.class, args);
 	}
 
 }
