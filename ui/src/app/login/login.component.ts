3b8c809eb57094fe5d7ca4ae28431e440d422a57
Sol Kumar <sohail.kumar03@gmail.com>
2022-11-19 17:10:26 -0500

    front end suer

---
+++ ui/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username: string = "hello";
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUser("V0")
+      .subscribe(user => this.username = user.username);
+  }
+
+}
