b52140520411f2117ea9502d4fa7a01264f29480
Zak Kaufman <Zak3148@rit.edu>
2022-11-19 14:44:24 -0500

    get Discount done in DiscountController

--- Api/src/main/java/com/csgame/api/csgameapi/controller/DiscountController.java
+++ Api/src/main/java/com/csgame/api/csgameapi/controller/DiscountController.java
@@ -16,5 +16,21 @@ public class DiscountController {
         this.discountDAO = discountDAO;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Discount> getDiscount(@PathVariable int id) {
+        LOG.info("GET /discounts/" + id);
+        try {
+            Discount discount = discountDAO.getDiscount(id);
+            if (discount != null)
+                return new ResponseEntity<Discount>(discount, HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     
 }
