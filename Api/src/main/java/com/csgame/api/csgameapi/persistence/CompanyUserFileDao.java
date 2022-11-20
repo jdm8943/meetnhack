a80c4147159a2df7e46d4f4f0b489b57c234a279
Sol Kumar <sohail.kumar03@gmail.com>
2022-11-19 19:41:51 -0500

    fix daos

--- Api/src/main/java/com/csgame/api/csgameapi/persistence/CompanyUserFileDao.java
+++ Api/src/main/java/com/csgame/api/csgameapi/persistence/CompanyUserFileDao.java
@@ -1,4 +1,87 @@
 package com.csgame.api.csgameapi.persistence;
-public class CompanyUserFileDao {
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.csgame.api.csgameapi.model.CompanyUser;
+
+public class CompanyUserFileDAO {
+    Map<String, CompanyUser> users;
+
+    private ObjectMapper objectMapper;
+
+    private String filename;
+
+    public CompanyUserFileDAO(@Value("${users.file}") String filename, ObjectMapper objectMapper) throws IOException {
+        this.filename = filename;
+        this.objectMapper = objectMapper;
+
+        load();
+    }
+
+    private CompanyUser[] getUsersArray() {
+        ArrayList<CompanyUser> userArray = new ArrayList<>();
+
+        for (CompanyUser u : users.values())
+            userArray.add(u);
+
+        CompanyUser[] array = (CompanyUser[]) userArray.toArray();
+
+        return array;
+    }
+
+    public CompanyUser getUser(String UID) {
+        synchronized (users) {
+            for (CompanyUser u : users.values())
+                if (u.getUID().equals(UID))
+                    return u;
+
+            return null;
+        }
+    }
+
+    public CompanyUser[] getUsers() {
+        synchronized (users) {
+            return getUsersArray();
+        }
+    }
+
+    private boolean save() throws IOException {
+        CompanyUser[] userArray = getUsersArray();
+
+        objectMapper.writeValue(new File(filename), userArray);
+        return true;
+    }
+
+    private boolean load() throws IOException {
+        users = new TreeMap<>();
+
+        CompanyUser[] userArray = objectMapper.readValue(new File(filename), CompanyUser[].class);
+
+        for (CompanyUser user : userArray)
+            users.put(user.getUsername(), user);
+
+        return true;
+    }
+
+    public CompanyUser login(String username, String password) {
+        for (CompanyUser u : users.values()) {
+            if (u.getUsername() == username && u.getPassword() == password)
+                return u;
+        }
+
+        return null;
+    }
+
+    public CompanyUser updateUser(CompanyUser user) {
+        return user;
+    }
 }
--- Api/src/main/java/com/csgame/api/csgameapi/persistence/VolunteerUserFileDAO.java
+++ Api/src/main/java/com/csgame/api/csgameapi/persistence/VolunteerUserFileDAO.java
@@ -1,5 +1,87 @@
 package com.csgame.api.csgameapi.persistence;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.csgame.api.csgameapi.model.VolunteerUser;
+
 public class VolunteerUserFileDAO {
-    
+    Map<String, VolunteerUser> users;
+
+    private ObjectMapper objectMapper;
+
+    private String filename;
+
+    public VolunteerUserFileDAO(@Value("${users.file}") String filename, ObjectMapper objectMapper) throws IOException {
+        this.filename = filename;
+        this.objectMapper = objectMapper;
+
+        load();
+    }
+
+    private VolunteerUser[] getUsersArray() {
+        ArrayList<VolunteerUser> userArray = new ArrayList<>();
+
+        for (VolunteerUser u : users.values())
+            userArray.add(u);
+
+        VolunteerUser[] array = (VolunteerUser[]) userArray.toArray();
+
+        return array;
+    }
+
+    public VolunteerUser getUser(String UID) {
+        synchronized (users) {
+            for (VolunteerUser u : users.values())
+                if (u.getUID().equals(UID))
+                    return u;
+
+            return null;
+        }
+    }
+
+    public VolunteerUser[] getUsers() {
+        synchronized (users) {
+            return getUsersArray();
+        }
+    }
+
+    private boolean save() throws IOException {
+        VolunteerUser[] userArray = getUsersArray();
+
+        objectMapper.writeValue(new File(filename), userArray);
+        return true;
+    }
+
+    private boolean load() throws IOException {
+        users = new TreeMap<>();
+
+        VolunteerUser[] userArray = objectMapper.readValue(new File(filename), VolunteerUser[].class);
+
+        for (VolunteerUser user : userArray)
+            users.put(user.getUsername(), user);
+
+        return true;
+    }
+
+    public VolunteerUser login(String username, String password) {
+        for (VolunteerUser u : users.values()) {
+            if (u.getUsername() == username && u.getPassword() == password)
+                return u;
+        }
+
+        return null;
+    }
+
+    public VolunteerUser updateUser(VolunteerUser user) {
+        return user;
+    }
 }
