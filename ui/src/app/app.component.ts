7fd65007d0e895c6a6e4a409352d2ef22bdd7309
sirpighead <jdm8943@rit.edu>
2022-11-20 10:23:56 -0500

    global username

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,20 @@ import { MenuItem } from 'primeng/api';
 })
 export class AppComponent {
   public loggedInID: string | null = null;
+  public username!: string
 
   title = 'Community Service Game';
 
+  constructor(
+    private userService: UserService
+  ) { }
+
   ngOnInit() {
     this.loggedIn();
+    console.log((this.loggedInID || '').toString());
+    this.userService.getUser((this.loggedInID || '').toString()).subscribe(user => {
+      this.username = user.username
+    })
   }
 
   loggedIn(): void {
@@ -23,6 +33,7 @@ export class AppComponent {
 
   logout(): void {
     localStorage.removeItem('loginSessId');
+    this.username = ''
     this.loggedIn();
   }
 }
