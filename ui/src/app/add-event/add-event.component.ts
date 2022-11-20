ad23f2604deb7babaf4d192b13c2feee2e58d721
Spencer <sjk7876@rit.edu>
2022-11-20 04:14:53 -0500

    Changed Event.ts to orgEvent.ts

--- ui/src/app/add-event/add-event.component.ts
+++ ui/src/app/add-event/add-event.component.ts
@@ -4,6 +4,8 @@ import { AppComponent } from '../app.component';
 
 import { Location } from '@angular/common';
 import { PrimeNGConfig } from 'primeng/api';
+import { EventService } from '../events.service';
+import { OrgEvent } from '../orgEvent';
 
 @Component({
   selector: 'app-add-event',
@@ -14,6 +16,7 @@ export class AddEventComponent implements OnInit {
 
   constructor(
     private primengConfig: PrimeNGConfig,
+    private eventService: EventService,
     private route: ActivatedRoute,
     private location: Location,
     public app: AppComponent
@@ -25,9 +28,8 @@ export class AddEventComponent implements OnInit {
   }
 
   add(name: string, description: string, points: number, date: string): void{
-    var classType = new String('Event');
     if (!name || !description || !points || !date) {return;}
-    
+    this.eventService.addEvent({name, description, points, date} as OrgEvent)
   }
 
   goBack(): void {
