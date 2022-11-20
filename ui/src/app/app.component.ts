77cebf7d3a4505d3d5e0ea5f574faa06b5ecbf7d
Sol Kumar <sohail.kumar03@gmail.com>
2022-11-20 04:42:33 -0500

    login bool works

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -10,32 +10,19 @@ import {MenuItem} from 'primeng/api';
 })
 export class AppComponent {
   loggedInID: string | null = null;
-  isAdmin: string | null = null;
 
   title = 'Community Service Game';
-  items: MenuItem[]=[];
 
   ngOnInit() {
-      this.items = [
-          {
-              label: 'Login',
-              url: 'http://localhost:4200/login'
-          },
-          {
-              label: 'Events',
-              url: 'http://localhost:4200/events'
-          }
-      ];
+      this.loggedIn();
   }
 
   loggedIn(): void{
     this.loggedInID = localStorage.getItem('loginSessId');
-    this.isAdmin = localStorage.getItem('isAdmin');
   }
 
   logout():void{
     localStorage.removeItem('loginSessId');
-    localStorage.removeItem('isAdmin');
     this.loggedIn();
   }
 }
