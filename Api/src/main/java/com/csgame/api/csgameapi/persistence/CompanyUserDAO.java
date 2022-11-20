76868670982fefb849ff9ffb972ac1882ba86103
Sol Kumar <sohail.kumar03@gmail.com>
2022-11-19 19:32:58 -0500

    DAO interface changes

--- Api/src/main/java/com/csgame/api/csgameapi/persistence/CompanyUserDAO.java
+++ Api/src/main/java/com/csgame/api/csgameapi/persistence/CompanyUserDAO.java
@@ -1,5 +1,22 @@
 package com.csgame.api.csgameapi.persistence;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.csgame.api.csgameapi.model.CompanyUser;
+
 public interface CompanyUserDAO {
-    
+    CompanyUser getUser(String username) throws IOException;
+
+    CompanyUser updateUser(CompanyUser updatedUser) throws IOException;
+
+    CompanyUser login(String username, String password);
 }
--- Api/src/main/java/com/csgame/api/csgameapi/persistence/CompanyUserFileDao.java
+++ Api/src/main/java/com/csgame/api/csgameapi/persistence/CompanyUserFileDao.java
@@ -1,5 +1,4 @@
 package com.csgame.api.csgameapi.persistence;
-
 public class CompanyUserFileDao {
-    
+
 }
